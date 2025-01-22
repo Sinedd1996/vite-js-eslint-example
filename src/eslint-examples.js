31554c02e11976ac20555c8b050e294e4cdd4b9e
i.gul <dell23qqq@mail.ru>
2025-01-22 18:15:50 +0300

    refactor: закомментировал код с ошибками, добавил husky, теперь при коммитах запускается линтер

--- src/eslint-examples.js
+++ src/eslint-examples.js
@@ -1,8 +1,7 @@
-// подключил husky при коммите, по этому код с ошибками закомментировал, при тестировании нужно раскомментировать
-
+// подключил husky при коммите, код с ошибками при тестировании нужно раскомментировать
 var exampleVar = 22;
-const exampleTernary = exampleVar ? 'example-true': 'example-false';
-const exampleEqeqeq = 1 == '1';
+// const exampleTernary = exampleVar ? 'example-true': 'example-false';
+// const exampleEqeqeq = 1 == '1';
 
 // export const getAlert = () => {
 //   alert('example alert');
